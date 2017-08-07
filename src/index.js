a0f904f1c5777c12ab71c36fa9f058bd661eedc7
Cem <cemozturan@gmail.com>
2017-08-07 21:29:44 +0100

    Adding react routes (/app, /sign-in, /sign-up) using react router (using version < 4 for browserHistory support)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+import { Router, Route, browserHistory } from 'react-router';
 
 ReactDOM.render(
-	<App />,
+	<Router path="/" history={browserHistory}>
+		<Route path="/app" component={App} />
+		<Route path="/sign-in" component={SignIn} />
+		<Route path="/sign-up" component={SignUp} />
+	</Router>,
 	document.getElementById('root'));
