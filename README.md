b19a33edd8582c36c247cf5b59bd42d1c6ff291b
Cem <cemozturan@gmail.com>
2017-08-07 23:34:24 +0100

    Implementing sign up functionality + Updating readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 "npm start" to run.
 
-You need to go to "firebase.google.com" and add a project. Then, go into "Add Firebase to your web app" and copy the content into the firebase.js file we have in the "src" folder.
+You need to go to "firebase.google.com" and add a project. Then, go into "Add Firebase to your web app" and copy the content into the firebase.js file we have in the "src" folder. Finally, go to "Authentication" on the left side menu on "firebase.google.com", go into "Sign-in Method" and enable "Email/Password".
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
+import { firebaseApp } from '../firebase';
 
 class SignUp extends Component {
-  render() {
-    return (
-      <div>
-		  Sign Up
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: '',
+			error: {
+				message: ''
+			}
+		}
+		this.signUp = this.signUp.bind(this);
+	}
+
+	signUp() {
+		const { email, password } = this.state;
+		firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+			.catch((error) => {
+				this.setState(() => {
+					return {
+						error
+					};
+				});
+			});
+	}
+
+  	render() {
+    	return (
+      		<div className="form-inline" style={{margin: '5%'}}>
+				<h2>Sign Up</h2>
+				<div className="form-group">
+					<input
+						className="form-control"
+						type="text"
+						placeholder="email"
+						style={{marginRight: '5%'}}
+						onChange={(event) => this.setState({email: event.target.value})}
+					/>
+					<input
+						className="form-control"
+						type="text"
+						placeholder="password"
+						style={{marginRight: '5%'}}
+						onChange={(event) => this.setState({password: event.target.value})}
+					/>
+					<button
+						className="btn btn-primary"
+						type="button"
+						onClick={this.signUp}
+					>
+						Sign Up
+					</button>
+				</div>
+				<div>{this.state.error.message}</div>
+			</div>
+    	);
+  	}
 }
 
 export default SignUp;
